97332a2ff6908f135bb4ccb578dbafdb94ca77d2
Dmitriy Nickolay Petruk <mistake952@mail.ru>
2018-11-12 19:00:23 +0200

    Disconnect handler (MySQL)

--- server.js
+++ server.js
@@ -34,6 +34,38 @@ con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 });
+
+function handleDisconnect() {
+  con = mysql.createConnection({
+  host: process.env.db_host,
+  user: process.env.db_user,
+  password: process.env.db_pass,
+  database: process.env.db_name
+});
+                                                  
+
+  connection.connect(function(err) {              
+    if(err) {                                     
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); 
+    }                                     
+  });                                    
+                                         
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+       throw err;                                 // server variable configures this)
+    });
+}
+
+handleDisconnect();
+
+
+
+
+
 function finish(userid, username)
 {
   if(users[userid].state != 2) return;
